cab8288237f6df7a819550584d82bd36832b09c2
mohittygithub <smartfriendmohit@gmail.com>
2021-03-11 21:07:46 +0530

    carrying on

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,36 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [data, setData] = useState([]);
   const getAllPosts = async () => {
-    const headers = {
-      'x-auth-token': localStorage.getItem('jwt'),
+    try {
+      const header = {
+        headers: { "x-auth-token": localStorage.getItem("jwt") },
+      };
+      const response = await axios.get(
+        `http://localhost:8080/posts/all`,
+        header
+      );
+      await setData(response.data.response);
+      const { tags, _id, title, author, user, createdAt } = data;
+      console.log(tags, _id, title, author, user, createdAt);
+    } catch (error) {
+      console.log(error.message);
     }
-    const response = await axios.get('/posts/all', headers)
-    console.log(response)
-  }
-  useEffect(() => {})
+    console.log(data);
+  };
+  useEffect(() => {
+    getAllPosts();
+  }, []);
   return (
     <>
-      <p>Home Page</p>
+      <ul>
+        {data.map((post) => (
+          <li key={post._id}>{post.author}</li>
+        ))}
+      </ul>
     </>
-  )
-}
-export default Home
+  );
+};
+export default Home;
